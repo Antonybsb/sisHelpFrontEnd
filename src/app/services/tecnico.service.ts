c0345ea2c29fd4d57cb59a5699868635c9c7a507
Antonybsb <artbsb1@gmail.com>
2023-11-06 19:46:16 -0300

    Componente Login

---
+++ src/app/services/tecnico.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Tecnico } from '../models/tecnico';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TecnicoService {
+
+  private baseUrl = "http://localhost:8080/tecnicos";
+
+  constructor(private http: HttpClient) { }
+
+  findById(id: any): Observable<Tecnico> {
+    return this.http.get<Tecnico>(`${this.baseUrl}/tecnicos/${id}`);
+  }
+
+  findAll(): Observable<Tecnico[]> {
+    return this.http.get<Tecnico[]>(`${this.baseUrl}/tecnicos`);
+  }
+
+  create(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.post<Tecnico>(`${this.baseUrl}/tecnicos`, tecnico);
+  }
+
+  update(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.put<Tecnico>(`${this.baseUrl}/tecnicos/${tecnico.id}`, tecnico);
+  }
+
+  delete(id: any): Observable<Tecnico> {
+    return this.http.delete<Tecnico>(`${this.baseUrl}/tecnicos/${id}`);
+  }
+}
